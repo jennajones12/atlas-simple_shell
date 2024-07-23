e6be181d46f18b8aec237ef0cf4693475a310072
Rain Nyte <rain_nyte@proton.me>
2024-07-23 10:46:40 -0500

    basic shell that receives an input from the user and then converts it into tokens. Only closes if user inputs "exit"

--- main.c
+++ main.c
@@ -2,16 +2,25 @@
 
 int main()
 {
-	char in[100] = {'\0'};
-	char read;
-	int end;
+	char in[100] = {'\0'}; //input string storage
+	char *drifter; //temporary storage of the individual tokens before assigning them to the array
+	char *toks[100]; //array of tokens converted from the input provided
+	int end; //used to store the result of strcmp for checking if the exit command is entered
+	size_t i; //iterative variable used for processing strtok
 
 	end = 1;
 
 	while(end != 0 )
 	{
 		printf("$: ");
-		read = scanf("%s",in);
+		scanf("%s",in);
+		drifter = strtok(in, " ");
+		i = 0;
+		while (drifter != NULL)
+		{
+			toks[i++] = drifter;
+			drifter = strtok(NULL, " ");
+		}
 		end = strcmp(in,"exit");
 	}
 	return(0);
