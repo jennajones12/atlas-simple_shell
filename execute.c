acd417132ba508eb326ed74be58f9cbb0fc44ad7
Jenna Jones <jljones121@gmail.com>
2024-08-16 03:31:31 -0500

    get env removed; revised with get_env_var

--- execute.c
+++ execute.c
@@ -9,9 +9,11 @@ extern char **environ;
 /* Custom function to get environment variable */
 char *get_env_var(const char *name) {
     int i;
+    size_t name_len = strlen(name);
+
     for (i = 0; environ[i] != NULL; i++) {
-        if (strncmp(environ[i], name, strlen(name)) == 0 && environ[i][strlen(name)] == '=') {
-            return environ[i] + strlen(name) + 1;
+        if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=') {
+            return environ[i] + name_len + 1;
         }
     }
     return NULL;
